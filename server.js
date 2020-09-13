b0c29919278bf896e449c26d1ad93f6b7dec8bba
cpcwood <cpcwood@outlook.com>
2020-09-13 22:06:03 +0100

    add base express server

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-import express from 'express'
+const debug = require('debug')('express:server')
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 5001
+const address = process.env.ADDRESS || '0.0.0.0'
+const environment = process.env.NODE_ENV || 'development'
+
+const server = app.listen(port, address, () => {
+  debug(`Starting dynamic SSR server in ${environment} mode`)
+  debug('Server is listening on port ' + port)
+  debug('Server is listening on address ' + address)
+})
+
+server.on('connection', socket => {
+  // 2 min timeout
+  socket.setTimeout(2 * 60 * 1000)
+})
+
+// graceful shutdown
+process.on('SIGTERM', () => {
+  debug('SIGTERM signal received: closing HTTP server')
+  server.close(() => {
+    debug('HTTP server closed')
+  })
+})
+
+process.on('SIGINT', () => {
+  debug('SIGINT signal received: closing HTTP server')
+  server.close(() => {
+    debug('HTTP server closed')
+  })
+})
