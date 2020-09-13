47c5cce638923d9a929f1672e39cd02d9bf418f1
cpcwood <cpcwood@outlook.com>
2020-09-13 23:26:20 +0100

    update resolveApiHost method to remove / on homepage visits

--- config/server.config.js
+++ config/server.config.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 
-const apiHostLocation = process.env.API_HOST_PORT ? 'local' : 'remote';
+const apiHostLocation = (process.env.API_HOST_PORT ? 'local' : 'remote');
 
 module.exports = {
   resolveApiHost(req) {
-    const port = process.env.API_HOST_PORT ? `:${process.env.API_HOST_PORT}` : '';
+    const port = (process.env.API_HOST_PORT ? `:${process.env.API_HOST_PORT}` : '');
+    const requestUrl = (req.originalUrl === '/' ? '' : req.originalUrl);
     if (process.env.API_HOST_LOCAL) {
-      return `localhost${port}${req.originalUrl}`;
+      return `localhost${port}${requestUrl}`;
     }
-    return `${process.env.API_PROTOCOL}://${process.env.API_HOST}${port}${req.originalUrl}`;
+    return `${process.env.API_PROTOCOL}://${process.env.API_HOST}${port}${requestUrl}`;
   },
   apiHostLocation,
 };
